5077af0bdead52303cc70c22d08159b1538a97d2
Ominuka Mauro <ominuka.mauro@gmail.com>
2021-04-15 17:22:17 -0300

    change the package.json and app.js. Adds library "cors" for request from the api of react. Es decir, in port diferently.

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser')
 const productRoutes = require('./routes/product')
 const path = require('path')
 const app = express()
+const cors = require('cors')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/v1', productRoutes)
 app.use('/public', express.static(`${path.resolve()}/files/imgs`))
-
+app.use(cors())
 module.exports = app
